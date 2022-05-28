cdaccc8eb2fecf74a11d4499c1d82defa1af3462
slimarc <sricardolimaa@gmail.com>
2022-05-28 09:22:18 -0300

    estilização bandeiras

---
+++ src/js/produtos.js
@@ -0,0 +1,62 @@
+const produtos = [
+    {
+        id:0,
+        nome: "Combo Whopper",
+        preco: 32.90,
+        image: "./src/assets/combo_whopper.png"
+    },
+    {
+        id:1,
+        nome: "BK Chicken",
+        preco: 16.90,
+        image: "./src/assets/bk_chicken.png"
+    },
+    {
+        id:2,
+        nome: "Sorvete",
+        preco: 9.80,
+        image: "./src/assets/sorvete.png"
+    },
+    {
+        id:3,
+        nome: "Cheddar Duplo",
+        preco: 13.90,
+        image: "./src/assets/cheddar_duplo.png"
+    },
+    {
+        id:4,
+        nome: "Combo Whopper Rodeio",
+        preco: 38.90,
+        image: "./src/assets/whopper_rodeio.png",
+    },
+    {
+        id:5,
+        nome: "Onion rings",
+        preco: 11.90,
+        image: "./src/assets/onion_rings.png"
+    },
+    {
+        id:6,
+        nome: "BK Mix Ovomaltine",
+        preco: 9.25,
+        image: "./src/assets/bk_mix.png"
+    },
+    {
+        id:7,
+        nome: "Cheddar",
+        preco: 11.90,
+        image: "./src/assets/cheddar.png"
+    },
+    {
+        id:8,
+        nome: "Shake Brownie",
+        preco: 11.35,
+        image: "./src/assets/shake.png"
+    },
+    {
+        id:9,
+        nome: "Combo Whopper rodeio",
+        preco: 32.40,
+        image: "./src/assets/combo_whopper.png"
+    },
+]
